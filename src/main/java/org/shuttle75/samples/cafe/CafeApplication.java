a80be19bce3c6b9360ef65f610c86fe963b9014f
Shuttle75 <Alexey.Osadchiy@mail.ru>
2017-10-05 18:36:38 +0300

    App

--- src/main/java/org/shuttle75/samples/cafe/CafeApplication.java
+++ src/main/java/org/shuttle75/samples/cafe/CafeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetClinicApplication {
+public class CafeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
+		SpringApplication.run(CafeApplication.class, args);
 	}
 }
